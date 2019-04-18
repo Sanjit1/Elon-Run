eb914b31f888e8c046b165c3666d31295c07091e
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-17 23:02:08 -0700

    todays work

--- Assets/mover.cs
+++ Assets/mover.cs
@@ -21,33 +21,17 @@ public class mover : MonoBehaviour
     private void Update()
     {
         Vector3 pos = transform.position;
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            for (int i = 0; i < 50; i++)
-            {
-                Invoke("W", 0.05f);   
-            }
-            }
-        if (Input.GetKeyDown(KeyCode.A)) { pos.x = pos.x - 2.5f; }
+        Quaternion rot = transform.rotation;
+        rot.z = 0;
+        if (Input.GetKeyDown(KeyCode.W)) { pos.y = pos.y + 3.4f; }
+        if (Input.GetKeyDown(KeyCode.A)) { pos.x = pos.x - 2f; }
         if (Input.GetKeyDown(KeyCode.S)) { pos.y = pos.y - 2.5f; }
-        if (Input.GetKeyDown(KeyCode.D)) { pos.x = pos.x + 2.5f; }
+        if (Input.GetKeyDown(KeyCode.D)) { pos.x = pos.x + 2f; }
         transform.position = pos;
+        transform.rotation = rot;
 
     }
 
-    public void W() {
-        Vector3 pos = transform.position;
-        pos.y = pos.y + 0.05f;
-        transform.position = pos;
-    }
-
-    private IEnumerator HandleIt()
-    {
-        Time.timeScale = 100;
-        beingHandled = true;
-        yield return new WaitForSeconds(1f);
-        beingHandled = false;
-       
-    }
+    
 
 }
