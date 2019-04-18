a9f4f5669b8a1354005b27d54b03fe42856248df
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-18 15:52:36 -0700

    Basically cameracontroller

---
+++ Assets/cameraMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMover : MonoBehaviour
+{
+    public Transform target;
+
+    void FixedUpdate()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.z = -50;
+        newPosition.x = newPosition.x + 6.23f;
+        newPosition.y = newPosition.y + 1.03f;
+        transform.position = newPosition;
+    }
+
+    
+}
