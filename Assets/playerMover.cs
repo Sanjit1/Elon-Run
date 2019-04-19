7a2a0df9e636eb9f11f3c13166834c4c37463f2f
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-18 20:24:27 -0700

    yup alot happened

---
+++ Assets/playerMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMover : MonoBehaviour
+{
+    public mover controller;
+    public float horizontalMove = 0f;
+    public bool jump = false;
+    public bool crouch = false;
+    public float runSpeed = 40f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
+ 
