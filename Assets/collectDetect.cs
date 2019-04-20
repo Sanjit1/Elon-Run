bb0611998b78a7716a7f2a5f8c30889b68b8a2b7
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-19 17:58:37 -0700

    yup

---
+++ Assets/collectDetect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectDetect : MonoBehaviour
+{
+    public Collision2D musk;
+    public GameObject Grimes;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } 
+
+
+    void OnCollisionEnter2D(Collision2D piece)
+    {
+        //if (piece == musk)
+        //{
+            Grimes.GetComponent<updateGrimes>().hitGrimes();
+        //}
+    }
+}
---
+++ Assets/updateGrimes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateGrimes : MonoBehaviour
+{
+    public Text grimes;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void hitGrimes()
+    {
+        score++;
+        grimes.text = score.ToString();
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
