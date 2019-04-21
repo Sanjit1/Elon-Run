0a69fd7f768ea64c76a239e92fa212a15cc286a3
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-20 18:14:38 -0700

    hahahaha

--- Assets/heWon.cs
+++ Assets/heWon.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class heWon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Collider2D muskColider;
+    public Transform muskTransform;
+    public Transform tesla; 
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D piece)
+    {
+
+
+        Vector3 newPosition = muskTransform.position;
+        newPosition.x = 70f;
+        newPosition.y = 80f;
+        transform.position = newPosition;
+        if (piece == muskColider)
+        {
+            muskTransform.position = newPosition;
+            newPosition.y = 75;
+            tesla.position = newPosition;
+        }
     }
 }
