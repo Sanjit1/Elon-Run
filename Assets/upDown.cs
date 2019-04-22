6c01c374aca40a556088bc0aee1442ebb80bd158
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-21 19:38:34 -0700

    Utah

--- Assets/upDown.cs
+++ Assets/upDown.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class upDown : MonoBehaviour
 {
+    bool upOrDown = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class upDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 posit = transform.position;
+        if (upOrDown)
+        {
+            posit.y += 0.1f;
+            if (posit.y == 7.5) upOrDown = false;
+        } else 
+        {
+            posit.y -= 0.1f;
+            if (posit.y == 2.5) upOrDown = false;
+        }
+        transform.position = posit;
     }
 }
