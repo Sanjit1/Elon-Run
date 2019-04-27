920fa59f81df97b4976261a1a947e7a0793a33ef
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-27 00:03:39 -0700

    hahahahahaa

--- Assets/elevatorTesla.cs
+++ Assets/elevatorTesla.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class elevatorTesla : MonoBehaviour
 {
+    public Transform musk;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D piece)
+    {
+        Vector2 pos = transform.position;
+        pos.x = 81;
+        pos.y = 30;
+        musk.position = pos; 
     }
 }
