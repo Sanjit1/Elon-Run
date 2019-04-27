6fa462c6fc9ab6d63b59a5243f3101448d96c558
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-26 23:38:07 -0700

    Almost to bed

--- Assets/updateScore.cs
+++ Assets/updateScore.cs
@@ -17,7 +17,10 @@ public class updateScore : MonoBehaviour
 
     public void FixedUpdate()
     {
-        if (muskieBoi.position.y > -3.2 && muskieBoi.position.y < 0.4) { score = Mathf.RoundToInt(muskieBoi.position.x) + 5; }
+        if (muskieBoi.position.y > -3.2 && muskieBoi.position.y < 3.0 ) { score = Mathf.RoundToInt(muskieBoi.position.x) + 5; }
+        if (muskieBoi.position.y > 3.05 && muskieBoi.position.y < 8.5 ) { score = 80 - Mathf.RoundToInt(muskieBoi.position.x) + 100; }
+        if (muskieBoi.position.y > 9.00 && muskieBoi.position.y < 14.0) { score = Mathf.RoundToInt(muskieBoi.position.x) + 205; }
+        if (muskieBoi.position.y > 14.0 && muskieBoi.position.y < 40.0) { score = 80 - Mathf.RoundToInt(muskieBoi.position.x) + 305; }
         scB.text = "Score " + score.ToString();
 
     }
