7005c60c8648757aeef00fd72593b401d7e0a196
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-04-27 22:21:07 -0700

    Create dolphinMove.cs

---
+++ Assets/dolphinMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dolphinMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public mover controller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(40 * Time.fixedDeltaTime, false, false);
+    }
+}
